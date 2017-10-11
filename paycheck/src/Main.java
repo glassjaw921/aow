a53e79a9cdd806c7168b9a3d7e54a6af9136e0c4
glassjaw921 <mike.impellizzeri@gmail.com>
2017-10-11 02:58:10 -0400

    Add files via upload

---
+++ paycheck/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+// MAIN --  [JESUS TAKE THE WHEEL]
+public class Main {
+
+    public static void main(String[] args) {
+
+    	// initialise cool variables
+    	double getDependants;
+    	double hoursWorked;
+		double grossPay;
+		double getTax;
+		double netPay;
+		double stateTax;
+		double federalTax;
+		double socialSecTax;
+		Scanner in = new Scanner(System.in);
+		paycheck pay = new paycheck();
+		employee employee = new employee();
+		hoursWorked = employee.getHoursWorked();
+
+
+
+		employee.setHoursWorked();
+		employee.setDependants();
+
+		// call accessors
+		getDependants = employee.getDependants();
+		hoursWorked = employee.getHoursWorked();
+
+
+		grossPay = pay.getGrossPay(hoursWorked, getDependants);
+		System.out.println("Gross Pay = $" + grossPay);
+		getTax = pay.getTax(grossPay);
+		System.out.println("Tax  = $" + getTax);
+		netPay = pay.netPay(grossPay, getTax, getDependants);
+		System.out.println("Your Net Pay = $" + netPay);
+		}
+
+
+
+
+
+
+
+
+}
