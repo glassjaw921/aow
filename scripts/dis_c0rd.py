ba8fa58e991873ad3265ab1831e1ce6804b1d55c
glassjaw921 <mike.impellizzeri@gmail.com>
2018-04-11 23:11:44 -0400

    Update dis_c0rd.py

--- scripts/dis_c0rd.py
+++ scripts/dis_c0rd.py
@@ -3,37 +3,32 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import pymysql
 
-
 bot_prefix = "!"
 client = commands.Bot(command_prefix=bot_prefix)
-token = ''
-HOST = ''
+token = ""  # this is for testnet
+HOST = '127.0.0.1'
 USER = ''
 PASSWORD = ''
-DB = ''
-PORT = 3306
+DB = 'poseidon'
 Bot.remove_command(client, "help")
-
 def connect():
     try:
-        connection = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=DB, port=PORT)
+        connection = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=DB)
         return connection
     except Exception as e:
         print(e)
 
-        
+
 @client.event
 async def on_ready():
     print("Bot online!")
     print("Name: {}".format(client.user.name))
     print("ID: {}".format(client.user.id))
-
-
 @client.command(pass_context=True)
+
 async def ping(ctx):
     await client.say("Pong!")
     print("PING/PONG")
-
 
 @client.command(pass_context=True)
 async def hug(ctx, message=None):
@@ -43,135 +38,158 @@ async def hug(ctx, message=None):
         await client.say("hugs {0}".format(message))
     print("HUG")
 
-
 @client.command(pass_context=True)
 async def help(ctx):
-    await client.say(""
-    "!help   \tFor HELP because you obviously need it"
+    await client.say("!help   \tFor HELP because you obviously need it"
     "\n!profile  Checks your profile or do !profile @username"
-    "\n!set  \t  To set your POSQ address. EXAMPLE : !set POSQ_ADDRESS_HERE "
-    "\n!new  \tTo update your current Poseidon address. EXAMPLE : !new POSQ_ADDRESS_HERE"
+    "\n!set  \t  Register with Bot. EXAMPLE : !set POSQ_ADDRESS"
+    "\n!new  \tTo update your current Poseidon address. EXAMPLE : !new POSQ_ADDRESS"
     "\n!ping \t Ping the bot"
-    "\n!hug  \t Hug's @userName")
+    "\n!hug  \t Hug's @userName"
+    "\n"
+    "\nAll commands start with !"
+    "\nYour POSQ address starts with a Q and is 34 characters long")
     print("HELP")
 
 
+
+
 @client.command(pass_context=True)
-async def set(ctx, message):
-    print("set called")
-    conn = connect()
-
-    # verify address length
-    if (len(message) != 34):
-        # address is invalid
-        msg = 'Invalid address. Address is not the correct length, must be 34 characters'
+async def set(ctx, message=None):
+    posqAddress = message
+    author = ctx.message.author
+    sql_id_number = ctx.message.author.id
+    if (message == None):
+        msg = 'invalid command. try again - !set POSEIDON_ADDRESS'
         await client.say(msg)
-        print(ctx.message.author, " tried invalid address")
+        print(author, "SET BLANK")
+    else:
+        conn = connect()
+        # verify address length
+        if (len(message) != 34):
+            # address is invalid
+            msg = 'Invalid address. Address is not the correct length, must be 34 characters'
+            await client.say(msg)
+            print(ctx.message.author, " tried invalid address")
 
-    elif (len(message) == 34):  # address is correct
-        # set users discord id number, posq address and message author name
+        elif (len(message) == 34):  # address is correct
+            # set users discord id number, posq address and message author name
 
-        posqAddress = message
-        author = ctx.message.author
-        sql_id_number = ctx.message.author.id
+            if message[:1] == 'Q':
+                # check to see if user already has a database entry
+                dbhandler = conn.cursor()
 
-        # check to see if user already has a database entry
-        dbhandler = conn.cursor()
-        try:
-            checkDB = dbhandler.execute("SELECT address from test WHERE user_id = '{}'".format(author))
-            currentPOSQAddress = dbhandler.fetchone()
+                try:
+                    checkDB = dbhandler.execute("SELECT address from poseidon WHERE user_id = '{}'".format(author))
+                    currentPOSQAddress = dbhandler.fetchone()
 
-            if (checkDB):
+                    if (checkDB): #USER ALREADY HAS A DATABASE ENTRY
 
-                msg = 'Your current POSQ address is  :  {} ' \
-                      '\nwould you like to set a new one? !new NEW_ADDRESS_HERE '.format(currentPOSQAddress[0])
-                await client.say(msg)
-                print(ctx.message.author, " ALREADY HAS AN ADDRESS")
-            else:
-                try:  # insert posq address into db
+                        msg = 'Your current POSQ address is  :  {} ' \
+                              '\nwould you like to set a new one? !new NEW_ADDRESS_HERE '.format(currentPOSQAddress[0])
+                        await client.say(msg)
+                        print(ctx.message.author, " ALREADY HAS AN ADDRESS")
 
 
-                    dbhandler.execute("INSERT INTO test (`user_id`, `address`, `id_number`, `total`) "
-                                      "VALUES ('{0}', '{1}', '<@{2}>', '100');".format(author, posqAddress, sql_id_number).encode('utf-8'))
-                    conn.commit()
-                    msg = '{0} has been set as your posq address'.format(posqAddress)
-                    await client.say(msg)
-                    print(author, "---- NEW DB USER CREATED ----")
+                    else:
+                        try:  # insert posq address into db
+                            dbhandler.execute("INSERT INTO poseidon (`user_id`, `address`, `id_number`, `total`) "
+                                              "VALUES ('{0}', '{1}', '<@{2}>', '100');".format(author, posqAddress, sql_id_number).encode('utf-8'))
+                            conn.commit()
+                            msg = '{0} has been set as your posq address'.format(posqAddress)
+
+                            await client.say(msg)
+                            print(author, "---- NEW DB USER CREATED ----")
+
+                        except Exception as e:
+                            print(e)
+
                 except Exception as e:
                     print(e)
-        except Exception as e:
-            print(e)
 
-    else:
-        msg = 'Invalid address. Please enter a Poseidon address beginning with Q'
-        await client.say(msg)
-        print(ctx.message.author, " tried invalid address")
+            else:
+                msg = 'Invalid address. Please enter a Poseidon address beginning with Q'
+                await client.say(msg)
+                print(author, " tried invalid NEW address")
+        else:
+            msg = 'Invalid address. Please enter a Poseidon address beginning with Q'
+            await client.say(msg)
+            print(ctx.message.author, " tried invalid address")
 
 
 @client.command(pass_context=True)
-async def new(ctx, message):
+async def new(ctx, message=None):
 
-    conn = connect()
     author = ctx.message.author
-    # verify address length
-    if (len(message) != 34):
-
-        # address is invalid
-        msg = 'Invalid address. Address is not the correct length, must be 34 characters'
+    sql_id_number = ctx.message.author.id
+    if (message == None):
+        msg = 'invalid command. try again - !new POSEIDON_ADDRESS'
         await client.say(msg)
-        print(author, " tried invalid NEW address")
-
-    elif (len(message) == 34):  # address is correct
-        # UPDATE user's discord
-        posqAddress = message
-        sql_id_number = ctx.message.author.id
-        dbhandler = conn.cursor()
-        checkdb = dbhandler.execute("SELECT address from test WHERE user_id = '{0}'".format(author))
-        print(author)
-        print(sql_id_number)
-
-        posqAddress = message
-        if (checkdb):
-
-            # check to see if user already has a database entry
-            try:  # insert POSQ address into db
-                dbhandler = conn.cursor()
-                dbhandler.execute("UPDATE test SET address = '{0}' WHERE id_number='<@{1}>'".format(posqAddress, sql_id_number))
-                conn.commit()
-                msg = '{0} has been set as your new POSQ address'.format(posqAddress)
-                await client.say(msg)
-                print(author, "posq address updated")
-
-            except Exception as e:
-
-                print(e)
-        else:
-            msg = '@{0} , you have no set an address yet'.format(author)
-            await client.say(msg)
-
-
+        print(author, "NEW BLANK")
     else:
-        msg = 'Invalid address. Please enter a Poseidon address beginning with Q'
-        await client.say(msg)
-        print(author, " tried invalid NEW address")
+        conn = connect()
+        # verify address length
+        if (len(message) != 34):
+
+            # address is invalid
+            msg = 'Invalid address. Address is not the correct length, must be 34 characters'
+            await client.say(msg)
+            print(author, " tried invalid NEW address")
+
+        elif (len(message) == 34):  # address is correct
+            # UPDATE user's discord
+            if message[:1] == 'Q':
+
+                dbhandler = conn.cursor()
+                checkdb = dbhandler.execute("SELECT address from test WHERE user_id = '{0}'".format(author))
+                posqAddress = message
+
+                if (checkdb): # USER ALREADY HAS A DATABASE ENTRY
+
+
+                    try:  # insert POSQ address into db
+                        dbhandler = conn.cursor()
+                        dbhandler.execute("UPDATE test SET address = '{0}' WHERE id_number='<@{1}>'".format(posqAddress, sql_id_number))
+                        conn.commit()
+                        msg = '{0} has been set as your new POSQ address'.format(posqAddress)
+                        await client.say(msg)
+                        print(author, "posq address updated")
+
+                    except Exception as e:
+                        print(e)
+                else:
+                    msg = '@{0} , you have no set an address yet'.format(author)
+                    await client.say(msg)
+                    print(author, " has not created an account yet")
+            else:
+                msg = 'Invalid address. Please enter a Poseidon address beginning with Q'
+                await client.say(msg)
+                print(author, " tried invalid NEW address")
+
+        else:
+            msg = 'Invalid address. Please enter a Poseidon address beginning with Q'
+            await client.say(msg)
+            print(author, " tried invalid NEW address")
 
 @client.command(pass_context=True)
 async def profile(ctx, message=None):
+
     if (message == None):
         conn = connect()
         author = ctx.message.author
+
         try:
-
-
             dbhandler = conn.cursor()
             checkdb = dbhandler.execute("SELECT address from test WHERE user_id = '{0}'".format(author))
             posqAddress = dbhandler.fetchone()
+
             if (checkdb): # USER HAS A DATABASE ENTRY
                 dbhandler.execute("SELECT total from test WHERE user_id = '{0}'".format(author))
                 posq = dbhandler.fetchone()
                 msg = 'User = @{0} \nPOSQ address = {1} \nPOSQ CREDIT = {2}'.format(author, posqAddress[0], posq[0])
                 await client.say(msg)
                 print(author, "checked profile")
+
             else:
                 msg = 'User {0} has not created a profile yet. use !help or !set'.format(author)
                 print(author, " NO PROFILE EXIST")
@@ -181,20 +199,23 @@ async def profile(ctx, message=None):
             print(e)
             msg = 'User {0} does not have a profile'.format(author)
             await client.say(msg)
+
     else:
         conn = connect()
         author = ctx.message.author
-        try:
 
+        try:
             dbhandler = conn.cursor()
             checkdb = dbhandler.execute("SELECT address from test WHERE id_number = '{0}'".format(message))
             posqAddress = dbhandler.fetchone()
+
             if (checkdb): # USER HAS A DATABASE ENTRY
                 dbhandler.execute("SELECT total from test WHERE id_number = '{0}'".format(message))
                 posq = dbhandler.fetchone()
                 msg = 'User = {0} \nPOSQ address = {1} \nPOSQ CREDIT = {2}'.format(message, posqAddress[0], posq[0])
                 await client.say(msg)
                 print(author, " checked ", message, " profile")
+
             else:
                 msg = 'User {0} has not created a profile yet. use !help or !set'.format(message)
                 print(message, " NO PROFILE EXIST")
@@ -204,5 +225,4 @@ async def profile(ctx, message=None):
             print(e)
             msg = 'User {0} does not have a profile'.format(author)
             await client.say(msg)
-            
 client.run(token)
